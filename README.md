e83bc7fbf3496542bf0dff0ea1125872597383fe
Haochi Chen <github@ihaochi.com>
2017-02-16 11:16:13 -0300

    Add readme

---
+++ README.md
@@ -0,0 +1,34 @@
+# Protobuf Stream
+
+This is used for reading and writing protobuf streams, specifically for messages that implements the following interface:
+
+```golang
+type Message interface {
+	proto.Marshaler
+	proto.Unmarshaler
+	proto.Sizer
+}
+```
+
+The official code generator [golang/protobuf](https://github.com/golang/protobuf) doesn't support `proto.Sizer`,
+but you can check out [gogo/protobuf](https://github.com/gogo/protobuf).
+
+## Write / WriteWithLock
+```golang
+if err := stream.Write(writer, message); err != nil {
+    log.Printf("Can't write: %v", err)
+}
+```
+
+## Read
+
+```golang
+for err != io.EOF {
+    var message Message
+    if err = stream.Read(reader, &message); err == nil {
+        log.Println(message)
+    } else if err != nil && err != io.EOF {
+        log.Println(err)
+    }
+}
+```
--- lib.go
+++ lib.go
@@ -18,12 +18,15 @@ type LockableWriter interface {
 	io.Writer
 }
 
+const bytesForSize = 8
+
+// Write writes the marshalled message into the writer
 func Write(w io.Writer, m Message) (err error) {
 	messageBytes, err := m.Marshal()
 
 	if err == nil {
 		size := m.Size()
-		sizeBytes := make([]byte, 8)
+		sizeBytes := make([]byte, bytesForSize)
 		binary.BigEndian.PutUint64(sizeBytes, uint64(size))
 
 		w.Write(sizeBytes)
@@ -33,12 +36,14 @@ func Write(w io.Writer, m Message) (err error) {
 	return
 }
 
+
+// WriteWithLock is just like Write, but will lock on write
 func WriteWithLock(w LockableWriter, m Message) (err error) {
 	messageBytes, err := m.Marshal()
 
 	if err == nil {
 		size := m.Size()
-		sizeBytes := make([]byte, 8)
+		sizeBytes := make([]byte, bytesForSize)
 		binary.BigEndian.PutUint64(sizeBytes, uint64(size))
 
 		w.Lock()
@@ -50,8 +55,9 @@ func WriteWithLock(w LockableWriter, m Message) (err error) {
 	return
 }
 
+// Read reads the message from the reader
 func Read(r io.Reader, m Message) (err error) {
-	sizeBytes  := make([]byte, 8)
+	sizeBytes  := make([]byte, bytesForSize)
 
 	if _, err = io.ReadFull(r, sizeBytes); err == nil {
 		size := binary.BigEndian.Uint64(sizeBytes)
